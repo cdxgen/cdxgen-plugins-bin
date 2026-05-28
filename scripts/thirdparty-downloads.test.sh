0d27c70cd54a2d5045eccdc5f8eb33763e7324fa
Prabhu Subramanian <prabhu@appthreat.com>
2026-05-28 18:08:50 +0530

    Fix test
    Signed-off-by: Prabhu Subramanian <prabhu@appthreat.com>

--- scripts/thirdparty-downloads.test.sh
+++ scripts/thirdparty-downloads.test.sh
@@ -12,11 +12,11 @@ asset_sha256=""
 resolve_asset dosai-linux-amd64
 [[ "$asset_filename" == "Dosai-linux-amd64" ]]
 [[ "$asset_url" == "https://github.com/owasp-dep-scan/dosai/releases/download/v${DOSAI_VERSION}/Dosai-linux-amd64" ]]
-[[ "$asset_sha256" == "8ac2ce891ab664a2887dec17eb902ef3d3b04a402cc00e64b81a3d349a82e7d0" ]]
+[[ "$asset_sha256" == "8963ea7bdb61fed81697ed2a85e6f567d111a2e36f767b8c213f3583196b249d" ]]
 
 resolve_asset dosai-windows-amd64
 [[ "$asset_filename" == "Dosai.exe" ]]
 [[ "$asset_url" == "https://github.com/owasp-dep-scan/dosai/releases/download/v${DOSAI_VERSION}/Dosai.exe" ]]
-[[ "$asset_sha256" == "826e6b142fdaf2286e2b44322f74e20281806cc46a0c1a466cb85203b9b2c708" ]]
+[[ "$asset_sha256" == "34fbbe401a6d62d127516ff1c1e145923a494698e788c0f0c2088e22a391aabc" ]]
 
 echo "thirdparty-downloads helper test passed"
